999e3f08439bff912dc1bc21f9b52568d2c936e7
sabina <UZBprogrammer30@gmail.com>
2024-12-10 05:55:28 +0500

    add tab elements

---
+++ js/script.js
@@ -0,0 +1,5 @@
+window.addEventListener('DOMContentLoaded', () =>{
+ const tabs = document.querySelectorAll('.tabheader__item'),
+ tabContents = document.querySelectorAll('.tab__content'),
+ tabParents = document.querySelectorAll('.tabheader__items')
+})
