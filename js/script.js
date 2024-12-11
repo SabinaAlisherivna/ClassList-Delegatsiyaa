ab0baa728f3958d9f15186007de68b1bcee74221
sabina <UZBprogrammer30@gmail.com>
2024-12-11 15:57:15 +0500

    all
    save

--- js/script.js
+++ js/script.js
@@ -2,4 +2,19 @@ window.addEventListener('DOMContentLoaded', () =>{
  const tabs = document.querySelectorAll('.tabheader__item'),
  tabContents = document.querySelectorAll('.tab__content'),
  tabParents = document.querySelectorAll('.tabheader__items')
+
+function hideTabContents(){
+ tabContents.forEach(tabContents =>{
+  tabContents.style.display = 'none'
+ })
+ tabs.forEach(tab =>{
+  tab.classList.remove('tabheader__item_active')
+ })
+}
+function showTabContent(index){
+ tabContents[index].style.display = 'flex'
+ tabs[index].classList.add('tabheader_item_active')
+}
+  hideTabContents()
+  showTabContent(1)
 })
